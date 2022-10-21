fd531388b8720726cfb8d28f3d555539c4be2f07
xwf <17703093412@163.com>
2022-10-21 11:45:26 +0800

    update all.py update test_01.py

--- pytest_learn/all.py
+++ pytest_learn/all.py
@@ -1,4 +1,4 @@
 import pytest
 
 if __name__ == '__main__':
-    pytest.main(['-vs','./testcase'])
+    pytest.main(['-vs','./testcase/test_01.py'])
--- pytest_learn/testcase/test_01.py
+++ pytest_learn/testcase/test_01.py
@@ -1,7 +1,20 @@
+from time import sleep
+
+
 class Test01:
 
     def test_01(self):
+        sleep(3)
         print('01')
 
     def test_02(self):
+        sleep(3)
         print('02')
+
+    def test_03(self):
+        sleep(3)
+        print('03')
+
+    def test_04(self):
+        sleep(3)
+        print("04")
